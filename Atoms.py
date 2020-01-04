acc8f87b1701d86213a3d1ed55bcc268bab98879
thomasdellaert <thomasdellaert@physics.ucla.edu>
2020-01-04 01:03:47 -0800

    Made a Grotrian class whose job is to make Grotrian diagrams. Moved all plotting logic from atoms to the Grotrian class.

--- Atoms.py
+++ Atoms.py
@@ -105,70 +105,38 @@ class EnergyLevel:
     #                       "y"            : [y], "x0": [x], "x1": [x1]})
     #             table = table.append(line, ignore_index=True)
 
-    def plottable_table(self, **kwargs):
-        params = {"width"            : 1.0,
-                  "sublevel_spacing" : 0.03,
-                  "zeeman"           : True,
-                  "compact"          : True,
-                  "hyperfine"        : True,
-                  "scale_splitting"  : 1.0,
-                  "override_position": False,
-                  "offset_position"  : (0.0, 0.0),
-                  "color"            : "black"}
-        for param in kwargs.keys():
-            params[param] = kwargs[param]
-        if not params["override_position"]:
-            x0 = self.J + params["offset_position"][0] - params["width"] / 2
-            y0 = self.level + params["offset_position"][1]
-        else:
-            x0, y0 = params["override_position"]
-
+    def data_table(self, hf=True, zeeman=True):
         table = DataFrame(columns=["configuration", "term", "level",
-                                   "J", "F", "m_F", "J_frac", "F_frac", "m_F_frac",
-                                   "color", "y0", "hf", "z",
-                                   "y", "x0", "x1"])
-        scale = params["scale_splitting"]
-
-        if not params["hyperfine"]:
-            x1 = x0 + params["width"]
+                                   "J", "F", "m_F", "J_frac", "F_frac", "m_F_frac", "hf", "z"])
+        if not hf:
             line = DataFrame(data={"configuration": [self.configuration], "term": [self.term], "level": self.level,
                                    "J"            : [self.J], "F": [None], "m_F": [None],
-                                   "J_frac"       : [term_frac(self.J)], "F_frac": [None], "m_F_frac": [None],
-                                   "color"        : [params["color"]], "y0": [y0], "hf": [0.0], "z": [0.0],
-                                   "y"            : [y0], "x0": [x0], "x1": [x1]})
-            table = table.append(line)
+                                   "J_frac"       : [term_frac(self.J)], "F_frac": [None],
+                                   "m_F_frac": [None],
+                                   "hf"           : [0.0], "z": [0.0]})
+            table = table.append(line, ignore_index=True)
         else:
             for F in self.Fs:
-                if not params["zeeman"]:
-                    y = y0 + self.hf_shifts[F] * scale
-                    hf = self.hf_shifts[F]
-                    level = self.level + self.hf_shifts[F]
-                    x1 = x0 + params["width"]
+                if not zeeman:
+                    hyperfine = self.hf_shifts[F]
+                    level = self.hf_levels[F]
                     line = DataFrame(data={"configuration": [self.configuration], "term": [self.term], "level": level,
                                            "J"            : [self.J], "F": [F], "m_F": [None],
                                            "J_frac"       : [term_frac(self.J)], "F_frac": [term_frac(F)],
                                            "m_F_frac"     : [None],
-                                           "color"        : [params["color"]], "y0": [y0], "hf": [hf], "z": [0.0],
-                                           "y"            : [y], "x0": [x0], "x1": [x1]})
+                                           "hf"           : [hyperfine], "z": [0.0]})
                     table = table.append(line, ignore_index=True)
                 else:
-                    delta = params["sublevel_spacing"]
-                    Fmax = max(self.Fs)
-                    wd = (params["width"] - delta * 2 * Fmax) / (2 * Fmax + 1)
                     for m_F in self.z_shifts[F].keys():
                         z = self.z_shifts[F][m_F]
-                        hf = self.hf_shifts[F]
-                        y = y0 + self.hf_shifts[F] * scale + self.z_shifts[F][m_F] * scale
+                        hyperfine = self.hf_shifts[F]
                         level = self.level + self.hf_shifts[F] + self.z_shifts[F][m_F]
-                        x = x0 + (m_F + Fmax) * (wd + delta)
-                        x1 = x + wd
                         line = DataFrame(
                             data={"configuration": [self.configuration], "term": [self.term], "level": level,
                                   "J"            : [self.J], "F": [F], "m_F": [m_F],
                                   "J_frac"       : [term_frac(self.J)], "F_frac": [term_frac(F)],
                                   "m_F_frac"     : [term_frac(m_F)],
-                                  "color"        : [params["color"]], "y0": [y0], "hf": [hf], "z": [z],
-                                  "y"            : [y], "x0": [x], "x1": [x1]})
+                                  "hf"           : [hyperfine], "z": [z]})
                         table = table.append(line, ignore_index=True)
         return table
 
@@ -181,70 +149,33 @@ class Transition:
         self.L_0, self.L_1 = level_0.L, level_1.L
         self.parity_0, self.parity_1 = level_0.parity, level_1.parity
 
-        self.transition_table = self.plottable_table()
+        self.transition_table = self.data_table()
 
         self.name = str(self.transition_table["term_0"]) + "->" + str(self.transition_table["term_1"])
 
-    def plottable_table(self, x_off_0=0.5, x_off_1=0.5, **kwargs):
-        params = {"scale_splitting"  : 1.0,
-                  "color"            : None}
-        for param in kwargs.keys():
-            params[param] = kwargs[param]
+    def data_table(self):
 
-        table_0 = self.level_0.plottable_table(scale_splitting = params["scale_splitting"])
-        table_1 = self.level_1.plottable_table(scale_splitting = params["scale_splitting"])
+        table_0 = self.level_0.data_table()
+        table_1 = self.level_1.data_table()
         line_0 = table_0.loc[(table_0["m_F"] == self.m_F_0) & (table_0["F"] == self.F_0)]
         line_1 = table_1.loc[(table_1["m_F"] == self.m_F_1) & (table_1["F"] == self.F_1)]
-        line_0 = line_0.drop(["color", "J_frac", "F_frac", "m_F_frac"], axis=1)
-        line_1 = line_1.drop(["color", "J_frac", "F_frac", "m_F_frac"], axis=1)
+        line_0 = line_0.drop(["J_frac", "F_frac", "m_F_frac"], axis=1)
+        line_1 = line_1.drop(["J_frac", "F_frac", "m_F_frac"], axis=1)
         if line_0.empty or line_1.empty:
             raise ValueError("The selected quantum numbers don't yield a transition. Check that they exist in the specified levels")
-        line_0.insert(9, "x", line_0["x0"]+x_off_0*(line_0["x1"]-line_0["x0"]))
-        line_1.insert(9, "x", line_1["x0"]+x_off_1*(line_1["x1"]-line_1["x0"]))
         line_0.columns = [str(col) + '_0' for col in line_0.columns]
         line_1.columns = [str(col) + '_1' for col in line_1.columns]
         line_0 = line_0.reset_index(drop=True)
         line_1 = line_1.reset_index(drop=True)
 
-        transition_table = pd.concat([line_0, line_1], axis=1, ignore_index=False)
+        data_table = pd.concat([line_0, line_1], axis=1, ignore_index=False)
 
-        def frequency_to_color(freq):
-            """
-            Takes a frequency (in Hertz for now) and outputs a hex color value based on the frequency
-            It does this by interpolating between set points where I know that the colors should be.
-            """
-            c = 3e8
-            wl = c / abs(float(freq)) * 1e9
-
-            wavelengths =   [0, 200, 280, 300, 350, 400, 445, 475, 493, 510, 570, 650, 780, 1000, 1500]
-            rs =            [0, 0,   0,   50,  120, 80 , 110, 0  , 40 , 40 , 255, 235, 90 , 50 , 0]
-            gs =            [0, 0,   0,   20,  120, 40 , 0  , 0  , 255, 255, 230, 30 , 30 , 20 , 0]
-            bs =            [0, 0,   0,   100, 255, 120, 255, 255, 250, 0  , 0  , 30 , 30 , 20 , 0]
-
-            rf = interpolate.interp1d(wavelengths, rs)
-            gf = interpolate.interp1d(wavelengths, gs)
-            bf = interpolate.interp1d(wavelengths, bs)
-
-            if wl < 1500:
-                r, g, b = rf(wl), gf(wl), bf(wl)
-            else:
-                r, g, b = 0, 0, 0
-
-            R = "{:02x}".format(int(r))
-            G = "{:02x}".format(int(g))
-            B = "{:02x}".format(int(b))
-
-            return "#" + R + G + B
-
-        delta_l = abs(transition_table["level_0"] - transition_table["level_1"])
-        if params["color"] is None:
-            params["color"] = frequency_to_color(delta_l*1e12)
+        delta_l = abs(data_table["level_0"] - data_table["level_1"])
         wavelength = 299792.458/delta_l
 
-        transition_table["delta_l"] = [delta_l]
-        transition_table["color"] = [params["color"]]
-        transition_table["wavelength"] = [wavelength]
-        return transition_table
+        data_table["delta_l"] = [delta_l]
+        data_table["wavelength"] = [wavelength]
+        return data_table
 
     def get_type(self):
         # TODO: finish this method sometime when I have time to actually fiure out what all the relevant selection rules are
@@ -273,7 +204,7 @@ class Atom:
             self.transitions[transition.name] = transition
         self.name = name
 
-    def add_level(self, *levels):
+    def add_level(self, levels):
         for level in levels:
             self.levels[level.name] = level
 
@@ -287,7 +218,7 @@ class Atom:
         else:
             raise ValueError("Unrecognized mode")
 
-    def add_transition(self, *transitions):
+    def add_transition(self, transitions):
         for transition in transitions:
             self.transitions[transition.name] = transition
 
--- Plots.py
+++ Plots.py
@@ -14,6 +14,9 @@ from bokeh.io import output_file, show
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.models import HoverTool, Label, LabelSet, Panel, Tabs, Slider, CustomJS
 from bokeh.layouts import row, column
+from scipy import interpolate
+from pandas import DataFrame
+import pandas as pd
 
 
 def build_reference_plot(df, nstates):
@@ -44,6 +47,215 @@ def build_reference_plot(df, nstates):
 
 # TODO: Make a Grotrian class with methods like g.transition, g.level, g.label_level, g.label_splitting. g.build_figure.
 # TODO: migrate any references to plotting from the atoms/levels/transitions and move it into the plotting classes.
+
+class Grotrian:
+    def __init__(self, atom, hf=True, zeeman=True):
+        self.hf=hf
+        self.zeeman=zeeman
+        self.atom = atom
+        self.plot_line_table = DataFrame(columns=["configuration", "term", "level",
+                                   "J", "F", "m_F", "J_frac", "F_frac", "m_F_frac",
+                                   "color", "y0", "hf", "z",
+                                   "y", "x0", "x1"])
+        self.plot_transition_table = DataFrame(columns = ["F_0", "J_0", "configuration_0", "hf_0", "level_0", "m_F_0",
+                                                     "term_0", "x0_0", "x1_0", "y_0", "y0_0", "z_0", "F_1", "J_1",
+                                                     "configuration_1", "hf_1", "level_1", "m_F_1", "term_1", "x0_1",
+                                                     "x1_1", "y_1", "y0_1", "z_1", "delta_l", "color", "wavelength"])
+
+    def level_table(self, level, width=1.0, sublevel_spacing=0.03, scale_splitting=1.0, override_position=False, 
+                    offset_position=(0.0, 0.0), color="black"):
+        table = DataFrame(columns=["configuration", "term", "level",
+                                   "J", "F", "m_F", "J_frac", "F_frac", "m_F_frac",
+                                   "color", "y0", "hf", "z",
+                                   "y", "x0", "x1"])
+        if not override_position:
+            x0 = level.J + offset_position[0] - width / 2
+            y0 = level.level + offset_position[1]
+        else:
+            x0, y0 = override_position
+            
+        scale = scale_splitting
+        
+        if not self.hf:
+            x1 = x0 + width
+            line = DataFrame(data={"configuration": [level.configuration], "term": [level.term], "level": level.level,
+                                   "J"            : [level.J], "F": [None], "m_F": [None],
+                                   "J_frac"       : [level.J_frac], "F_frac": [None], "m_F_frac": [None],
+                                   "color"        : [color], "y0": [y0], "hf": [0.0], "z": [0.0],
+                                   "y"            : [y0], "x0": [x0], "x1": [x1]})
+            table = table.append(line, ignore_index=True)
+        else:
+            for F in level.Fs:
+                if not self.zeeman:
+                    hf = level.hf_shifts[F]
+                    y = y0 + hf * scale
+                    level = level.hf_levels[F]
+                    x1 = x0 + width
+                    line = DataFrame(data={"configuration": [level.configuration], "term": [level.term], "level": level,
+                                           "J"            : [level.J], "F": [F], "m_F": [None],
+                                           "J_frac"       : [level.J_frac], "F_frac": [level.F_frac], "m_F_frac": [None],
+                                           "color"        : [color], "y0": [y0], "hf": [hf], "z": [0.0],
+                                           "y"            : [y], "x0": [x0], "x1": [x1]})
+                    table = table.append(line, ignore_index=True)
+                else:
+                    delta = sublevel_spacing
+                    F_max = max(level.Fs)
+                    wd = (width - delta*2*F_max)/(2*F_max+1)
+                    for m_F in level.z_shifts[F].keys():
+                        z = level.z_shifts[F][m_F]
+                        hf = level.hf_shifts[F]
+                        y = y0 + hf*scale + z*scale
+                        level = level.z_levels[F][m_F]
+                        x = x0 + (m_F + F_max) * (wd + delta)
+                        x1 = x + wd
+                        line = DataFrame(
+                            data={"configuration": [level.configuration], "term": [level.term], "level": level,
+                                  "J"            : [level.J], "F": [F], "m_F": [m_F],
+                                  "J_frac"       : [level.J_frac], "F_frac": [level.F_frac], "m_F_frac": [level.m_F_frac],
+                                  "color"        : [color], "y0": [y0], "hf": [hf], "z": [z],
+                                  "y"            : [y], "x0": [x], "x1": [x1]})
+                        table = table.append(line, ignore_index=True)
+        return table
+    
+    def transition_table(self, transition, x_off_0=0.5, x_off_1=0.5, scale_splitting=1.0, color=None):
+        table_0 = self.level_table(transition.level_0, scale_splitting=scale_splitting)
+        table_1 = self.level_table(transition.level_1, scale_splitting=scale_splitting)
+        line_0 = table_0.loc[(table_0["m_F"] == self.m_F_0) & (table_0["F"] == self.F_0)]
+        line_1 = table_1.loc[(table_1["m_F"] == self.m_F_1) & (table_1["F"] == self.F_1)]
+        line_0 = line_0.drop(["color", "J_frac", "F_frac", "m_F_frac"], axis=1)
+        line_1 = line_1.drop(["color", "J_frac", "F_frac", "m_F_frac"], axis=1)
+        if line_0.empty or line_1.empty:
+            raise ValueError("The selected quantum numbers don't yield a transition. Check that they exist in the specified levels")
+        line_0.insert(9, "x", line_0["x0"]+x_off_0*(line_0["x1"]-line_0["x0"]))
+        line_1.insert(9, "x", line_1["x0"]+x_off_1*(line_1["x1"]-line_1["x0"]))
+        line_0.columns = [str(col) + '_0' for col in line_0.columns]
+        line_1.columns = [str(col) + '_1' for col in line_1.columns]
+        line_0 = line_0.reset_index(drop=True)
+        line_1 = line_1.reset_index(drop=True)
+
+        transition_table = pd.concat([line_0, line_1], axis=1, ignore_index=False)
+
+        def frequency_to_color(freq):
+            """
+            Takes a frequency (in Hertz for now) and outputs a hex color value based on the frequency
+            It does this by interpolating between set points where I know that the colors should be.
+            """
+            c = 3e8
+            wl = c / abs(float(freq)) * 1e9
+
+            wavelengths =   [0, 200, 280, 300, 350, 400, 445, 475, 493, 510, 570, 650, 780, 1000, 1500]
+            rs =            [0, 0,   0,   50,  120, 80 , 110, 0  , 40 , 40 , 255, 235, 90 , 50 , 0]
+            gs =            [0, 0,   0,   20,  120, 40 , 0  , 0  , 255, 255, 230, 30 , 30 , 20 , 0]
+            bs =            [0, 0,   0,   100, 255, 120, 255, 255, 250, 0  , 0  , 30 , 30 , 20 , 0]
+
+            rf = interpolate.interp1d(wavelengths, rs)
+            gf = interpolate.interp1d(wavelengths, gs)
+            bf = interpolate.interp1d(wavelengths, bs)
+
+            if wl < 1500:
+                r, g, b = rf(wl), gf(wl), bf(wl)
+            else:
+                r, g, b = 0, 0, 0
+
+            R = "{:02x}".format(int(r))
+            G = "{:02x}".format(int(g))
+            B = "{:02x}".format(int(b))
+
+            return "#" + R + G + B
+
+        delta_l = abs(transition_table["level_0"] - transition_table["level_1"])
+        if color is None:
+            color = frequency_to_color(delta_l*1e12)
+        wavelength = 299792.458/delta_l
+
+        transition_table["delta_l"] = [delta_l]
+        transition_table["color"] = [color]
+        transition_table["wavelength"] = [wavelength]
+        return transition_table
+
+    def add_level(self, levels, **kwargs):
+        for level in levels:
+            self.plot_line_table = self.plot_line_table.append(self.level_table(level, **kwargs))
+        return self.plot_line_table()
+
+    def add_transition(self, transitions, **kwargs):
+        for transition in transitions:
+            self.plot_transition_table = self.plot_transition_table(self.transition_table(transition, **kwargs))
+        return self.plot_transition_table()
+
+    def build_figure(self, dimensions=(800, 1000), y_range=(-1e2, 1e3), title=None, zeeman=True, hf=True, scale_splitting=3000):
+        p = figure(title=title, plot_width=dimensions[0], plot_height=dimensions[1], y_range=y_range, x_range=(0, 4))
+        line_source = ColumnDataSource(self.plot_line_table)
+        arrow_source = ColumnDataSource(self.plot_transition_table)
+        lines = p.segment(x0="x0", y0="y", x1="x1", y1="y",
+                          color="color", source=line_source)
+        arrows = p.segment(x0="x_0", y0="y_0", x1="x_1", y1="y_1",
+                           color="color", line_width=3, source=arrow_source)
+        # TODO: Maybe make the arrows arrow-y? Might be more trouble than it's worth
+
+        hover_lines = HoverTool(tooltips=[("Term", "@term @J_frac F=@F_frac, m_F=@m_F_frac"),
+                                          ("Level", "@level{0.000000}")], renderers=[lines])
+        hover_arrows = HoverTool(tooltips=[("Frequency", "@delta_l{0.000000} THz"),
+                                           ("Wavelength", "@wavelength{0.00} nm")], renderers=[arrows])
+        p.add_tools(hover_lines)
+        p.add_tools(hover_arrows)
+
+        scale_slider = Slider(start=1, end=10000, value=scale_splitting, step=10, title="Scaling")
+        b_field_slider = Slider(start=0, end=100, value=5, step=0.01, title="B-field (G)")
+
+        line_callback = CustomJS(args=dict(source=line_source, scale=scale_slider, b_field=b_field_slider), code="""
+                var data = source.data;
+                var b_field = b_field.value;
+                var scale = scale.value;
+
+                hf = data['hf'];
+                z = data['z'];
+                y = data['y'];
+                y0 = data['y0'];
+                level = data['level'];
+
+                for (i=0; i < y.length; i++) {
+                    y[i] = hf[i]*scale + z[i]*b_field*scale + y0[i];
+                    level[i] = y0[i] + hf[i] + z[i]*b_field;
+                };       
+                source.change.emit();
+            """)
+
+        arrow_callback = CustomJS(args=dict(source=arrow_source, scale=scale_slider, b_field=b_field_slider), code="""
+                var data = source.data;
+                var b_field = b_field.value;
+                var scale = scale.value;
+
+                hf0 = data['hf_0'];
+                hf1 = data['hf_1'];
+                z0 = data['z_0'];
+                z1 = data['z_1'];
+                y0 = data['y_0'];
+                y1 = data['y_1'];
+                y01 = data['y0_1'];
+                y00 = data['y0_0'];
+                level0 = data['level_0'];
+                level1 = data['level_1'];
+                delta_l = data['delta_l'];
+
+                for (i=0; i < y0.length; i++) {
+                    y0[i] = hf0[i]*scale + z0[i]*b_field*scale + y00[i];
+                    y1[i] = hf1[i]*scale + z1[i]*b_field*scale + y01[i];
+                    level0[i] = y00[i] + hf0[i] + z0[i]*b_field;
+                    level1[i] = y01[i] + hf1[i] + z1[i]*b_field;
+                    delta_l[i] = level1[i] - level0[i]
+                };
+                source.change.emit();
+            """)
+
+        scale_slider.js_on_change('value', line_callback)
+        scale_slider.js_on_change('value', arrow_callback)
+        b_field_slider.js_on_change('value', line_callback)
+        b_field_slider.js_on_change('value', arrow_callback)
+
+        show(row(p, column(scale_slider, b_field_slider)))
+
+        return row(p, column(scale_slider, b_field_slider))
 
 def build_grotrian_diagram(atom, dimensions=(800, 1000), y_range=(-1e2, 1e3),
                            title=None, zeeman=True, hf=True, scale=3000, show_plot=True):
@@ -153,6 +365,12 @@ def build_final_plots(atoms, **kwargs):
 
 if __name__ == "__main__":
     from Atom_Library import Yb_171, Yb_173, Yb_174
+
+    g = Grotrian(Yb_173)
+    g.add_level(Yb_173.list_levels().values())
+    g.add_transition(Yb_173.list_transitions().values())
+
+    g.build_figure()
 
     build_grotrian_diagram(Yb_173, show_plot=True)
 
