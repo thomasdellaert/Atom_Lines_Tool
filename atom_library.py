e4a478735f76ce5ac42f0e3f50738a6e5b27d3f9
thomasdellaert <thomasdellaert@physics.ucla.edu>
2020-01-28 23:31:36 -0800

    Atom Library now takes coeffs from a csv

--- atom_library.py
+++ atom_library.py
@@ -1,23 +1,35 @@
 from atoms import *
 from parsers import parse_NIST_levels
+from pandas import read_csv
 
 df_Yb = parse_NIST_levels("YbII_NIST_levels.csv")
 
 
-# TODO: allow this to take its A and B coeffs from a csv.
-def _populate_levels(df, atom, I=0.0, default_A=0.0, default_B=0.0, n_levels=-1):
+def _populate_levels(df, atom, I=0.0, default_A=0.0, default_B=0.0, n_levels=-1, hf_source=None):
     ls = []
+    hfs = None
     if n_levels == -1:
         n_levels = len(df["index"])
+    if I != 0 and hf_source is not None:
+        hfs = read_csv(hf_source, index_col=0)
     for i in range(n_levels):
         try:
-            level = EnergyLevel(df, i, I=I, A_coeff=default_A, B_coeff=default_B)
+            level = EnergyLevel(df, i, I=I)
+            if hfs is not None:
+                try:
+                    level.A_coeff = hfs.at[level.name, "A_coeff"]
+                    level.B_coeff = hfs.at[level.name, "B_coeff"]
+                except KeyError:
+                    level.A_coeff = default_A
+                    level.B_coeff = default_B
             ls.append(level)
         except Exception as e:
             print e.__doc__
             print e.message
     atom.add_level(ls)
+    atom.rezero()
 
+# TODO: populate_transitions()
 
 # region define 174Yb
 Yb_174 = Atom(name="174Yb")
@@ -27,24 +39,7 @@ Yb_174.rezero()
 
 # region define 173Yb
 Yb_173 = Atom(name="173Yb")
-_populate_levels(df_Yb, Yb_173, I=2.5, n_levels=30, default_A=0.01e-3, default_B=0.0)
-
-Yb_173.levels["2S1/2"].A_coeff = -3.4975e-3
-Yb_173.levels["2P*1/2"].A_coeff = -0.5812e-3
-Yb_173.levels["2P*3/2"].A_coeff = -0.245e-3
-Yb_173.levels["2F*7/2"].A_coeff = -0.24e-3
-Yb_173.levels["2D3/2"].A_coeff = -0.11031e-3
-Yb_173.levels["2D5/2"].A_coeff = 0.00347e-3
-Yb_173.levels["3[3/2]*5/2"].A_coeff = -0.055e-3
-
-Yb_173.levels["2S1/2"].B_coeff = 0.0
-Yb_173.levels["2P*1/2"].B_coeff = 0.0
-Yb_173.levels["2P*3/2"].B_coeff = 1.46e-3
-Yb_173.levels["2F*7/2"].B_coeff = 4.762e-3
-Yb_173.levels["2D3/2"].B_coeff = 0.09514e-3
-Yb_173.levels["2D5/2"].B_coeff = 1.1904e-3
-Yb_173.levels["3[3/2]*5/2"].B_coeff = -1.72e-3
-Yb_173.rezero()
+_populate_levels(df_Yb, Yb_173, I=2.5, n_levels=30, default_A=0.01e-3, default_B=0.0, hf_source="173Yb_Hyperfine.csv")
 
 _t = [
     Transition(level_0=Yb_173.levels["2D5/2"], F_0=4, m_F_0=0, level_1=Yb_173.levels["2P*3/2"], F_1=3, m_F_1=0),
@@ -56,16 +51,7 @@ Yb_173.add_transition(_t)
 
 # region define 171Yb
 Yb_171 = Atom(name="171Yb")
-_populate_levels(df_Yb, Yb_171, I=0.5, n_levels=30, default_A=0.01e-3)
-Yb_171.levels["2S1/2"].A_coeff = 12.642812e-3
-Yb_171.levels["2P*1/2"].A_coeff = 2.1079e-3
-Yb_171.levels["2P*3/2"].A_coeff = 0.8754e-3
-Yb_171.levels["2F*7/2"].A_coeff = 1.105e-3
-Yb_171.levels["2D3/2"].A_coeff = 0.40048e-3
-Yb_171.levels["2D5/2"].A_coeff = -0.01258e-3
-Yb_171.levels["3[3/2]*5/2"].A_coeff = 0.199e-3
-Yb_171.levels["1[3/2]*3/2"].A_coeff = 4.45e-3
-Yb_171.rezero()
+_populate_levels(df_Yb, Yb_171, I=0.5, n_levels=30, default_A=0.01e-3, hf_source="171Yb_Hyperfine.csv")
 
 _t = [
     Transition(level_0=Yb_171.levels["2S1/2"], F_0=1, m_F_0=0, level_1=Yb_171.levels["2P*1/2"], F_1=1, m_F_1=0),
@@ -81,6 +67,5 @@ Yb_171.add_transition(_t)
 
 
 if __name__ == "__main__":
-    print Yb_173.levels.keys()
     for level in Yb_173.levels.values():
-        print level.A_coeff
+        print level.name, level.A_coeff
