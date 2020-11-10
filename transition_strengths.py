d45ad335ad039211b406681a1687356499aa1cf7
Thomas Dellaert <dellaert.thomas@gmail.com>
2020-11-10 14:38:44 -0800

    pep8

--- transition_strengths.py
+++ transition_strengths.py
@@ -1,6 +1,5 @@
 import py3nj as nj
 import numpy as np
-import math
 
 
 # noinspection PyTypeChecker
@@ -22,10 +21,11 @@ def tkq_LS_transition_strength(I, k, q, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
                                  int(J1 * 2), int(J0 * 2), int(S0 * 2)) ** 2 *
                      nj.wigner6j(int(J0 * 2), int(J1 * 2), int(k * 2),
                                  int(F1 * 2), int(F0 * 2), int(I * 2)) ** 2 *
-                     nj.wigner3j(int(F1 * 2),  int(k * 2), int(F0 * 2),
+                     nj.wigner3j(int(F1 * 2), int(k * 2), int(F0 * 2),
                                  int(-M1 * 2), int(q * 2), int(M0 * 2)) ** 2)
     else:
         return 0.0
+
 
 # The tensor math for E2 (and somewhat E1) transitions is adapted from Tony's thesis (Ransford 2020)
 
@@ -37,15 +37,17 @@ def E1_transition_strength_geom(eps, I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
     tot += tkq_LS_transition_strength(I, 1, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * 0.5 * (eps[0] + eps[1]) ** 2
     return tot
 
+
 def E1_transition_strength_avg(I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
-    if (L1-L0)%2 == 1:
+    if (L1 - L0) % 2 == 1:
         tot = 0
-        tot += tkq_LS_transition_strength(I, 1, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
-        tot += tkq_LS_transition_strength(I, 1, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
-        tot += tkq_LS_transition_strength(I, 1, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
+        tot += tkq_LS_transition_strength(I, 1, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
+        tot += tkq_LS_transition_strength(I, 1, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
+        tot += tkq_LS_transition_strength(I, 1, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
         return tot
     else:
         return 0
+
 
 def M1_transition_strength_geom(eps, I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
     eps = eps / np.linalg.norm(eps)
@@ -58,12 +60,13 @@ def M1_transition_strength_geom(eps, I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
     else:
         return 0
 
+
 def M1_transition_strength_avg(I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
     tot = 0
     if S1 == S0 and L1 == L0:
-        tot += tkq_LS_transition_strength(I, 1, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
-        tot += tkq_LS_transition_strength(I, 1, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
-        tot += tkq_LS_transition_strength(I, 1, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0/3.0)
+        tot += tkq_LS_transition_strength(I, 1, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
+        tot += tkq_LS_transition_strength(I, 1, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
+        tot += tkq_LS_transition_strength(I, 1, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (1.0 / 3.0)
         return tot
     else:
         return 0
@@ -76,24 +79,25 @@ def E2_transition_strength_geom(eps, k, I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M
         print "k-vector and polarization are not orthogonal"
     tot = 0
     tot += tkq_LS_transition_strength(I, 2, -2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * \
-           (eps[0] ** 2 + eps[1] ** 2) * (k[0] ** 2 + k[1] ** 2)
+        (eps[0] ** 2 + eps[1] ** 2) * (k[0] ** 2 + k[1] ** 2)
     tot += tkq_LS_transition_strength(I, 2, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * \
-           (eps[2] * k[0] + eps[0] * k[2]) ** 2 + (eps[1] * k[0] + eps[0] * k[1]) ** 2
+        (eps[2] * k[0] + eps[0] * k[2]) ** 2 + (eps[1] * k[0] + eps[0] * k[1]) ** 2
     tot += tkq_LS_transition_strength(I, 2, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * \
-           (2. / 3.) * (3 * k[0] * eps[0] + 3 * k[1] * eps[1] + 2 * k[2] * eps[2]) ** 2
+        (2. / 3.) * (3 * k[0] * eps[0] + 3 * k[1] * eps[1] + 2 * k[2] * eps[2]) ** 2
     tot += tkq_LS_transition_strength(I, 2, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * \
-           (eps[2] * k[0] + eps[0] * k[2]) ** 2 + (eps[1] * k[0] + eps[0] * k[1]) ** 2
+        (eps[2] * k[0] + eps[0] * k[2]) ** 2 + (eps[1] * k[0] + eps[0] * k[1]) ** 2
     tot += tkq_LS_transition_strength(I, 2, 2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * \
-           (eps[0] ** 2 + eps[1] ** 2) * (k[0] ** 2 + k[1] ** 2)
+        (eps[0] ** 2 + eps[1] ** 2) * (k[0] ** 2 + k[1] ** 2)
     return tot
+
 
 def E2_transition_strength_avg(I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
     tot = 0
-    tot += tkq_LS_transition_strength(I, 2, -2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (3.0/29.0)
-    tot += tkq_LS_transition_strength(I, 2, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (9.0/58.0)
-    tot += tkq_LS_transition_strength(I, 2, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (14.0/29.0)
-    tot += tkq_LS_transition_strength(I, 2, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (9.0/58.0)
-    tot += tkq_LS_transition_strength(I, 2, 2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (4.0/15.0)
+    tot += tkq_LS_transition_strength(I, 2, -2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (3.0 / 29.0)
+    tot += tkq_LS_transition_strength(I, 2, -1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (9.0 / 58.0)
+    tot += tkq_LS_transition_strength(I, 2, 0, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (14.0 / 29.0)
+    tot += tkq_LS_transition_strength(I, 2, 1, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (9.0 / 58.0)
+    tot += tkq_LS_transition_strength(I, 2, 2, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1) * (4.0 / 15.0)
     return tot
 
 
@@ -101,23 +105,26 @@ def E2_transition_strength_avg(I, L0, S0, J0, F0, M0, L1, S1, J1, F1, M1):
 
 # TODO: Check the geometric factors on the averages for the E1 and M1 transitions
 
+# TODO: Make this work outside of LS coupling. Probably involves converting
+#  between LS, JJ, and JK couplings in a different .py file
+
 if __name__ == '__main__':
-    I = 2.5
-    L0 = 3
-    L1 = 3
-    S0 = 0.5
-    S1 = 0.5
-    J0 = 3.5
-    J1 = 3.5
-    F0 = 4
-    F1 = 3
+    I_0 = 2.5
+    L_0 = 3
+    L_1 = 3
+    S_0 = 0.5
+    S_1 = 0.5
+    J_0 = 3.5
+    J_1 = 3.5
+    F_0 = 4
+    F_1 = 3
     G = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
     E = [-3, -2, -1, 0, 1, 2, 3]
     print "M1"
     flg = False
     for mg in G:
         for me in E:
-            s = M1_transition_strength_avg(I, L0, S0, J0, F0, mg, L1, S1, J1, F1, me)
+            s = M1_transition_strength_avg(I_0, L_0, S_0, J_0, F_0, mg, L_1, S_1, J_1, F_1, me)
             if s != 0:
                 flg = True
                 print "{0:.6f} {1:} {2:}".format(s, mg, me)
@@ -127,7 +134,7 @@ if __name__ == '__main__':
     flg = False
     for mg in G:
         for me in E:
-            s = E1_transition_strength_avg(I, L0, S0, J0, F0, mg, L1, S1, J1, F1, me)
+            s = E1_transition_strength_avg(I_0, L_0, S_0, J_0, F_0, mg, L_1, S_1, J_1, F_1, me)
             if s != 0:
                 flg = True
                 print "{0:.6f} {1:} {2:}".format(s, mg, me)
